5b1e1f8f1dec26915597884618f965b81949bc2a
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-02-01 19:50:46 +0300

    save current index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import axios from 'axios';
+import cheerio from 'cheerio';
 import { findKey, has } from 'lodash';
 
-const createFileName = (pageUrl) => {
+const createFileName = (pageUrl, ending = '') => {
   const { hostname, pathname } = new URL(pageUrl);
   const pathParts = pathname.split('/').filter(w => w.length > 0);
 
   const hostParts = hostname.split('.');
-  return `${[...hostParts, ...pathParts].join('-')}.html`;
+  return `${[...hostParts, ...pathParts].join('-')}${ending}`;
 };
 
 const errorTypeSelector = {
@@ -22,14 +23,53 @@ const createMessage = (e = { message: '' }) => {
   return errType ? errorTypeSelector[errType](e) : errorTypeSelector.unknown(e);
 };
 
-const loadPage = (pageUrl, targetDir) => axios.get(pageUrl)
-  .then(res => res.data)
-  .then((data) => {
-    const filePath = path.join(targetDir, createFileName(pageUrl));
-    return fs.writeFile(filePath, data, 'utf8');
-  })
-  .catch((e) => {
-    throw new Error(createMessage(e));
-  });
+const attrToTagTable = {
+  img: 'src',
+  link: 'href',
+  script: 'src',
+};
+
+const localResourse = /^\/\w.*/; // matches local recourses
+
+const loadPage = (pageUrl, targetDir) => {
+  let $;
+  let resourses;
+  const resoursesDirName = createFileName(pageUrl, '_files');
+  const resoursesDirPath = path.join(targetDir, resoursesDirName);
+  return fs.mkdir(resoursesDirPath)
+    .then(() => axios.get(pageUrl))
+    .then((res) => {
+      $ = cheerio.load(res.data);
+      resourses = $('link,script,img').map((i, elem) => {
+        const node = $(elem);
+        const [{ name }] = node.get();
+        const url = node.attr(attrToTagTable[name]);
+        return { node, url };
+      }).get().filter(({ url }) => localResourse.test(url));
+      return Promise.all(resourses
+        .map(({ url }) => {
+          const resUrl = new URL(pageUrl);
+          resUrl.pathname = url;
+          return axios(/* {
+            method: 'get',
+            url: resUrl.href,
+            responseType: 'stream',
+          } */resUrl.href,
+          );
+        }));
+    })
+    .then(loadedResourses => Promise.all(loadedResourses
+      // TODO implement create file name more intelligent
+      .map((res, i) => fs.writeFile(
+        path.join(targetDir, resoursesDirName, `${resourses[i].url.pathname}`), res, 'utf8',
+      ))))
+    .then(() => {
+      const filePath = path.join(targetDir, createFileName(pageUrl, '.html'));
+      return fs.writeFile(filePath, $.html(), 'utf8');
+    })
+    .catch((e) => {
+      throw new Error(createMessage(e));
+    });
+};
 
 export default loadPage;
