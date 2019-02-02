e2e7ba25779f4fab17fc445d2d5a662c9c7f5b08
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-02-02 09:49:02 +0300

    get jest to ignore '.js' files in __fixtures__ dir

--- __tests__/pageloader.test.js
+++ __tests__/pageloader.test.js
@@ -7,7 +7,6 @@ import loadPage from '../src';
 
 const createResoursesTuples = (resoursesDir, resoursesPaths) => resoursesPaths
   .map((resPath) => {
-    const [, ...pathParts] = resPath.split('/');
     const fileName = tail(resPath.split('/')).join('-');
     return [resPath, join(resoursesDir, fileName)];
   });
@@ -30,7 +29,7 @@ beforeAll(async () => {
   nock.disableNetConnect();
   fixtures.url = new URL('https://jestjs.io');
   const fixtureDir = '__tests__/__fixtures__/';
-  fixtures.localHtmlData = await fs.readFile(join(fixtureDir, 'localized.html'), 'utf8');
+  fixtures.localHtmlData = await fs.readFile(join(fixtureDir, 'localized.html'));
   const pathFileTuples = [
     ['/en/help', 'jestjs-io-en-help.html'],
     ...createResoursesTuples('jestjs-io-en-help_files',
@@ -45,9 +44,10 @@ beforeAll(async () => {
       ]),
   ];
   const resousesData = await Promise.all(
-    pathFileTuples.map(([, filePath]) => fs.readFile(join(fixtureDir, filePath), 'utf8')),
+    pathFileTuples.map(([, filePath]) => fs.readFile(join(fixtureDir, filePath))),
   );
-  fixtures.resourses = pathFileTuples.map((tuple, i) => [...tuple, resousesData[i]])
+  fixtures.resourses = pathFileTuples
+    .map((tuple, i) => [...tuple, resousesData[i]])
     .map(([urlPath, filePath, data]) => ({ urlPath, filePath, data }));
 });
 
@@ -56,27 +56,31 @@ beforeEach(async () => {
 });
 
 
-test('load data to file', async () => {
+test('load data to files', async () => {
   nock(fixtures.url.origin)
     .get(path => getResoursePath(path))
     .times(fixtures.resourses.length)
-    .reply(200, path => getResourseData(path));
+    .reply(200, (path) => {
+      const data = getResourseData(path);
+      return data;
+    });
 
   const {
     urlPath: urlRootHtmlPath,
     filePath: createdRootHtmlPath,
   } = head(fixtures.resourses);
-
   const targetUrl = createUrlByPath(urlRootHtmlPath);
   await loadPage(targetUrl.href, fixtures.targetDir);
-  const createdHtmlData = await fs.readFile(join(fixtures.targetDir, createdRootHtmlPath), 'utf8');
-  expect(createdHtmlData).toBe(fixtures.localHtmlData);
+  const createdHtmlData = await fs.readFile(
+    join(fixtures.targetDir, createdRootHtmlPath),
+    'utf8',
+  );
+  expect(createdHtmlData).toBe(fixtures.localHtmlData.toString());
 
-  const resoursesData = await Promise.all(fixtures.resourses
-    .map(({ filePath }) => fs.readFile(join(fixtures.targetDir, filePath), 'utf8')));
+  const resoursesData = await Promise.all(tail(fixtures.resourses)
+    .map(({ filePath }) => fs.readFile(join(fixtures.targetDir, filePath))));
   resoursesData.forEach((data, i) => {
-    console.log(tail(fixtures.resourses)[i].urlPath);
-    expect(data).toBe(tail(fixtures.resourses)[i].data);
+    expect(Buffer.compare(data, tail(fixtures.resourses)[i].data)).toBe(0);
   });
 });
 
